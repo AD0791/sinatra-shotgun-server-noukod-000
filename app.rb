96ad8e0f3cd0588fdfaf0af2002a9e86c69a5a6a
Alexandro Disla <alexandrodisla@hotmail.com>
2019-06-28 22:44:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Hello World, my first sinatra app with Shotgun instead of rackup!!!!\n
-
-    controller "
+    "Hello World, my first sinatra app with Shotgun instead of rackup!!!!\n controller "
   end
 
 end
